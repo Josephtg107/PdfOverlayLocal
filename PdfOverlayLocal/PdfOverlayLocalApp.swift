db57bbcbc6d9849d0352f3116faf96fea1265255
Jose Teodoro Garcia <josephtg107@gmail.com>
2024-04-24 20:18:53 -0500

    Initial Commit

---
+++ PdfOverlayLocal/PdfOverlayLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfOverlayLocalApp.swift
+//  PdfOverlayLocal
+//
+//  Created by Joseph Garcia on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PdfOverlayLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
